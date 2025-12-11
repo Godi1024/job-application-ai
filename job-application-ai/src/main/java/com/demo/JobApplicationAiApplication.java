511989a705417b40109c1197c683e89bc763f4d5
Vinesh Godishala <guptavinesh1024@gmail.com>
2025-12-11 14:43:21 +0530

    first commit

---
+++ job-application-ai/src/main/java/com/demo/JobApplicationAiApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationAiApplication.class, args);
+	}
+
+}
